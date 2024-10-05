f71e529828a2a8860c69c1f7987c980d4961344a
Pasha <pashaalbara01@gmail.com>
2024-10-05 15:06:44 +0700

    first commit

---
+++ src/data/resume.tsx
@@ -0,0 +1,273 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, NotebookIcon } from "lucide-react";
+
+export const DATA = {
+  name: "Muhamad Pasha Albara",
+  initials: "MPA",
+  url: "https://mhdpasha.vercel.app",
+  location: "Jakarta, ID",
+  locationLink: "https://www.google.com/maps/place/jakarta",
+  description:
+    "Web App Engineer, loves to do and build things digitally.",
+  summary:
+    "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
+  avatarUrl: "/me.png",
+  skills: [
+    "React",
+    "Next.js",
+    "Typescript",
+    "Node.js",
+    "Python",
+    "Laravel",
+    "Livewire",
+    "Postgres",
+    "MySQL",
+    "Figma",
+  ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    { href: "/blog", icon: NotebookIcon, label: "Blog" },
+  ],
+  contact: {
+    email: "pashaalbara01@gmail.com",
+    tel: "+62 857 7751 1106",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/mhdpasha",
+        icon: Icons.github,
+
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://linkedin.com/in/mhdpasha",
+        icon: Icons.linkedin,
+
+        navbar: true,
+      },
+      X: {
+        name: "X",
+        url: "https://dub.sh/dillion-twitter",
+        icon: Icons.x,
+
+        navbar: false,
+      },
+      Youtube: {
+        name: "Youtube",
+        url: "https://dub.sh/dillion-youtube",
+        icon: Icons.youtube,
+        navbar: false,
+      },
+      email: {
+        name: "Send Email",
+        url: "mailto:pashaalbara01@gmail.com",
+        icon: Icons.email,
+
+        navbar: true,
+      },
+    },
+  },
+
+  work: [
+    {
+      company: "Redtop Hotel & Convention Center",
+      href: "#detail",
+      badges: [],
+      location: "Full time",
+      title: "Full Stack Developer",
+      logoUrl: "/redtop.png",
+      start: "Jul 2024",
+      end: "Sep 2024",
+      description:
+        "Implemented the Bitcoin discreet log contract (DLC) protocol specifications as an open source Typescript SDK. Dockerized all microservices and setup production kubernetes cluster. Architected a data lake using AWS S3 and Athena for historical backtesting of bitcoin trading strategies. Built a mobile app using react native and typescript.",
+    },
+    {
+      company: "Agung Toyota Automandiri",
+      href: "#detail",
+      badges: [],
+      location: "Hybrid",
+      title: "Full Stack Developer",
+      logoUrl: "/agung-toyota.jpg",
+      start: "Mar 2023",
+      end: "Sep 2023",
+      description:
+        "Implemented the Bitcoin discreet log contract (DLC) protocol specifications as an open source Typescript SDK. Dockerized all microservices and setup production kubernetes cluster. Architected a data lake using AWS S3 and Athena for historical backtesting of bitcoin trading strategies. Built a mobile app using react native and typescript.",
+    },
+  ],
+  education: [
+    {
+      school: "Universitas Terbuka",
+      href: "https://www.ut.ac.id",
+      degree: "Bachelor's Degree of Applied Science, Sistem Informasi",
+      logoUrl: "/ut.png",
+      start: "2024",
+      end: "Present",
+    },
+    {
+      school: "SMKN 40 Jakarta",
+      href: "https://smknegeri40-jkt.sch.id/",
+      degree: "Highschool Diploma, Rekayasa Perangkat Lunak",
+      logoUrl: "/smkn-40.png",
+      start: "2021",
+      end: "2024",
+    },
+  ],
+  projects: [
+    {
+      title: "Chat Collect",
+      href: "https://chatcollect.com",
+      dates: "Jan 2024 - Feb 2024",
+      active: true,
+      description:
+        "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Stripe",
+        "Shadcn UI",
+        "Magic UI",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://chatcollect.com",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
+    },
+    {
+      title: "Magic UI",
+      href: "https://magicui.design",
+      dates: "June 2023 - Present",
+      active: true,
+      description:
+        "Designed, developed and sold animated UI components for developers.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Stripe",
+        "Shadcn UI",
+        "Magic UI",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://magicui.design",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/magicuidesign/magicui",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "https://cdn.magicui.design/bento-grid.mp4",
+    },
+    {
+      title: "llm.report",
+      href: "https://llm.report",
+      dates: "April 2023 - September 2023",
+      active: true,
+      description:
+        "Developed an open-source logging and analytics platform for OpenAI: Log your ChatGPT API requests, analyze costs, and improve your prompts.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Shadcn UI",
+        "Magic UI",
+        "Stripe",
+        "Cloudflare Workers",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://llm.report",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/dillionverma/llm.report",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "https://cdn.llm.report/openai-demo.mp4",
+    },
+    {
+      title: "Automatic Chat",
+      href: "https://automatic.chat",
+      dates: "April 2023 - March 2024",
+      active: true,
+      description:
+        "Developed an AI Customer Support Chatbot which automatically responds to customer support tickets using the latest GPT models.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Shadcn UI",
+        "Magic UI",
+        "Stripe",
+        "Cloudflare Workers",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://automatic.chat",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/automatic-chat.mp4",
+    },
+  ],
+  hackathons: [
+    {
+      title: "AWS Sagasitas C3 Website Development",
+      dates: "May 19th - 21st, 2017",
+      location: "Province - DKI Jakarta",
+      description: "Improved PocketDoc and submitted to online competition",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/make-school-hackathon.png",
+      win: "Top 10 Finalist | Honourable Mention",
+      links: [
+        {
+          title: "Medium Article",
+          icon: <Icons.globe className="h-4 w-4" />,
+          href: "https://medium.com/make-school/the-winners-of-make-schools-student-app-competition-2017-a6b0e72f190a",
+        },
+        {
+          title: "Devpost",
+          icon: <Icons.globe className="h-4 w-4" />,
+          href: "https://devpost.com/software/pocketdoc-react-native",
+        },
+        {
+          title: "YouTube",
+          icon: <Icons.youtube className="h-4 w-4" />,
+          href: "https://www.youtube.com/watch?v=XwFdn5Rmx68",
+        },
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/dillionverma/pocketdoc-react-native",
+        },
+      ],
+    },
+  ],
+} as const;
